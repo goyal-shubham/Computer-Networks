356bac59a0d7390873e9f1bd23ab8fef424d22a0
Shubham Goyal <tango2890@gmail.com>
2016-05-07 10:54:19 -0700

    Final changes with Binary file transfer update

--- src/SFTP_TCP/TCP_Server.java
+++ src/SFTP_TCP/TCP_Server.java
@@ -13,98 +13,102 @@ public class TCP_Server {
     public static void main(String[] args) {
 
         try {
+
+            //Initializing ServerSocket
             ServerSocket server = new ServerSocket(8090);
             System.out.println("Waiting for the Client....");
 
+            //Waiting for clients to connect
             Socket client = server.accept();
             System.out.println("Connection Established....");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-//            int n = Integer.parseInt(in.readLine());
-            int n = 1;
-
+            //Get output file name from client
             String output = in.readLine();
-
             System.out.println("Output file name " + output);
 
+            //Setting output stream for file
             File file1 = new File(output);
             FileOutputStream fos = new FileOutputStream(file1);
 
+            //Get total file size
             int size1 = Integer.parseInt(in.readLine());
             System.out.println("File size  = " + size1);
 
-            if( n == 1)
+            //To know how many times client will send the data
+            int count = Integer.parseInt(in.readLine());
+            System.out.println("Number of operations at server " + count);
+
+            byte array1[];
+            byte array2[];
+
+
+            for(int i = 0; i < count; i++)
             {
+                int i1 = Integer.parseInt(in.readLine());
+                char[] arr = new char[i1];
+                in.read(arr, 0, i1);
+                String message = new String(arr);
 
-                int count = Integer.parseInt(in.readLine());
-                System.out.println("Number of operations at server " + count);
+                System.out.println("message = " + message);
+                int size = message.length();
+                System.out.println("size = " +size);
+                System.out.println("Data sequences in chunk of 5s: ");
 
-                byte array1[];
-                byte array2[];
-
-
-                for(int i = 0; i < count; i++)
+                //If size is greater than 5, then we need to divide the messgae in two array.
+                if(size <= 10 && size > 5)
                 {
-                    int i1 = Integer.parseInt(in.readLine());
-                    char[] arr = new char[i1];
-                    in.read(arr, 0, i1);
-                    String message = new String(arr);
-
-                    System.out.println("message = " + message);
-                    int size = message.length();
-//                    System.out.println("size = " + size);
-                    System.out.println("size = " +size);
-                    System.out.println("Data sequences in chunk of 5s: ");
-                    if(size <= 10 && size > 5)
+                    array1 = new byte[5];
+                    array2 = new byte[size - 5];
+                    int j;
+                    for( j = 0; j < 5 ; j++)
                     {
-                        array1 = new byte[5];
-                        array2 = new byte[size - 5];
-                        int j;
-                        for( j = 0; j < 5 ; j++)
-                        {
-                            array1[j] = (byte) message.charAt(j);
-                            System.out.print(message.charAt(j));
-                        }
-                        System.out.println();
-                        for(int k = 0; k < size  - 5; k++,j++)
-                        {
-                            array2[k] = (byte) message.charAt(j);
-                            System.out.print(message.charAt(j));
-                        }
-                        System.out.println();
-                        fos.write(array1);
-                        fos.write(array2);
+                        array1[j] = (byte) message.charAt(j);
+                        System.out.print(message.charAt(j));
                     }
-                    else
+                    System.out.println();
+                    for(int k = 0; k < size  - 5; k++,j++)
                     {
-                        array1 = new byte[size];
-                        for(int j = 0; j < size ; j++)
-                        {
-                            array1[j]= (byte) message.charAt(j);
-                            System.out.print(message.charAt(j));
-
-                        }
-                        fos.write(array1);
+                        array2[k] = (byte) message.charAt(j);
+                        System.out.print(message.charAt(j));
                     }
-                    in.readLine();
-
-                }
-                System.out.println("Transfer complete");
-                in.close();
-
-                if(fos!=null)
-                {
-                    fos.close();
+                    System.out.println();
+                    fos.write(array1);
+                    fos.write(array2);
                 }
 
-                client.close();
-                server.close();
+                //For message size less than 5. Only one array is needed.
+                else
+                {
+                    array1 = new byte[size];
+                    for(int j = 0; j < size ; j++)
+                    {
+                        array1[j]= (byte) message.charAt(j);
+                        System.out.print(message.charAt(j));
+
+                    }
+                    fos.write(array1);
+                }
+                in.readLine();
+
+            }
+            System.out.println("Transfer complete");
+
+            //Closing connecions and streams.
+            in.close();
+
+            if(fos!=null)
+            {
+                fos.close();
             }
 
+            client.close();
+            server.close();
 
 
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
